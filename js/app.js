a43a214342ad9a8c6cb5298a798dd336e2d5bf2c
yousef-97 <alshenyousef@gmail.com>
2020-03-30 19:50:58 +0300

    jQuery Done

---
+++ js/app.js
@@ -0,0 +1,55 @@
+'use strict';
+const keywordArr = [];
+$.get('../data/page-1.json')
+.then (data=>{
+    data.forEach(value=>{
+        let newOne = new HornAnimals(value.title,value.description,value.horns,value.keyword,value.image_url);
+        newOne.renderAll();
+        if(keywordArr.includes(newOne.keyword)){
+            console.log('alreadyHere');
+        }
+        else{
+            keywordArr.push(newOne.keyword);
+            let optionCopy = $('.choose').clone();
+            optionCopy.removeClass('choose');
+            optionCopy.text(newOne.keyword);
+            optionCopy.attr('value',newOne.keyword);
+            $('#horns').append(optionCopy);
+        }
+    });    
+    $('.content').remove();
+    $('.choose').remove();
+});
+function HornAnimals(title,description,horns,keyword,image_url){
+    this.title = title;
+    this.description = description;
+    this.horns = horns;
+    this.keyword = keyword;
+    this.image_url = image_url;
+}
+
+HornAnimals.prototype.renderAll = function(){
+
+    let theCopy = $('.content').clone();
+    theCopy.removeClass('content');
+    theCopy.addClass('all');
+    theCopy.addClass(this.keyword);
+
+    theCopy.find('h3').text(this.title);
+    theCopy.find('img').addClass('theImg');
+    theCopy.find('img').attr('src',this.image_url);
+    theCopy.find('figcaption').text(this.description);
+    $('#shown').append(theCopy);
+
+
+}
+$('select').on('change',function(){
+    console.log($(this));
+    console.log($(this).val());
+    let selected = $('select option:selected').val();
+    $('figure').hide();
+    console.log($('select option:selected').val());
+        $(`.${selected}`).show();
+
+
+})
